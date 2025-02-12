aad7c13ba97913f7e8c50d3c67e0d4e7af2f9e70
John Jenkins <john.jenkins@nanoporetech.com>
2025-02-12 14:19:44 +0000

    first commit

---
+++ src/components/my-component.test.tsx
@@ -0,0 +1,16 @@
+import { h } from '@stencil/core';
+import { render } from '@wdio/browser-runner/stencil';
+import { expect } from '@wdio/globals';
+
+
+describe('nano-accordion', () => {
+  beforeEach(async () => {
+    render({
+      template: () => <my-component />,
+    });
+  });
+
+  it('testing things', async () => {
+    expect(true).toBe(true);
+  });
+});
