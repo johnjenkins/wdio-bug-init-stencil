1b15bc20bd63a6596e32d50f2334fde99aae8ce7
John Jenkins <john.jenkins@nanoporetech.com>
2025-02-12 14:20:34 +0000

    chore:

--- src/components/my-component.test.tsx
+++ src/components/my-component.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@wdio/browser-runner/stencil';
 import { expect } from '@wdio/globals';
 
 
-describe('nano-accordion', () => {
+describe('testing-stuff', () => {
   beforeEach(async () => {
     render({
       template: () => <my-component />,
